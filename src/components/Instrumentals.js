6e32c6230f071cb69d6b0071f04ed4f898b17ac0
Rolin Blake <rolinmblake@gmail.com>
2023-08-31 01:23:15 -0500

    added new EP iframe and updated some styling

--- src/components/Instrumentals.js
+++ src/components/Instrumentals.js
@@ -6,20 +6,22 @@ const Instrumentals = () => {
     <div id='yt-playlists-wrap' className='page-wrap'>
       <h2>instrumental EPs</h2>
       <div id='iframes-wrap'>
+        <h4>ignorant, jealous, useless // uploaded 8/31/2023</h4>
+        <iframe src="https://www.youtube.com/embed/zqkdPyx_64M" title="ignorant, jealous, useless" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>consistency // uploaded 7/11/2023</h4>
-	      <iframe src="https://www.youtube.com/embed/jwToqrJ4HZw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	      <iframe src="https://www.youtube.com/embed/jwToqrJ4HZw" title="consistency" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>unusually calm // uploaded 6/3/2023</h4>
-        <iframe src="https://www.youtube.com/embed/jJSzmE5diHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/jJSzmE5diHQ" title="unusually calm" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>past and present // uploaded 4/11/2023</h4>
-        <iframe src="https://www.youtube.com/embed/0OX1J3uB78I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/0OX1J3uB78I" title="past and present" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>what's my worth // uploaded 4/5/2023</h4>
-        <iframe src="https://www.youtube.com/embed/yZ473RO8ZOo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/yZ473RO8ZOo" title="what's my worth" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>choose your fate // uploaded 3/27/2023</h4>
-        <iframe src="https://www.youtube.com/embed/8xe_cuXxXvw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/8xe_cuXxXvw" title="choose your fate" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>last fall // uploaded 3/4/2023</h4>
-        <iframe src="https://www.youtube.com/embed/I-vYkgP0QsQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/I-vYkgP0QsQ" title="last fall" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>up to speed // uploaded 2/21/2023</h4>
-        <iframe src="https://www.youtube.com/embed/d5UakBfp1MU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/d5UakBfp1MU" title="up to speed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
   )
