68631e796c4ea5b786be328b8d4759cb8746e474
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-10-17 07:05:25 -0500

    adding defense mechanism iframe to instrumentals page

--- src/components/Instrumentals.js
+++ src/components/Instrumentals.js
@@ -6,25 +6,8 @@ const Instrumentals = () => {
     <div id='yt-playlists-wrap' className='page-wrap'>
       <h2>instrumental EPs / music videos</h2>
       <div id='iframes-wrap'>
-        <iframe width="100%" height="100" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1875364464&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-        <div style={{
-          fontSize: '10px',
-          color: '#cccccc',
-          lineBreak: 'anywhere',
-          wordBreak: 'normal',
-          overflow: 'hidden',
-          whiteSpace: 'nowrap',
-          textOverflow: 'ellipsis',
-          fontFamily: 'Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda, Verdana, Tahoma, sans-serif',
-          fontWeight: 100
-        }}>
-          <a href="https://soundcloud.com/re_detach" title="re detach" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>
-            re detach
-          </a> · 
-          <a href="https://soundcloud.com/re_detach/dumb-live-remix-playlist-to-go-nonverbal-to" title="dumb live remix playlist to go nonverbal to" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>
-            dumb live remix playlist to go nonverbal to
-          </a>
-        </div>
+        <h4>defense mechanism // uploaded 10/17/2024</h4>
+        <iframe src="https://www.youtube.com/embed/lL0nKe9KTOE?si=fjk5PuYeh57bTRE3" title="defense mechanism" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>cognitive dissonance // uploaded 6/5/2024</h4>
         <iframe src="https://www.youtube.com/embed/9qhxvALfs78?si=kjVL4aI2B9QQ-rQ9" title="cognitive dissonance" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h4>get what you deserve // uploaded 2/4/2024</h4>
