f18e333ba0706d0d2ee36ec69030deb63077f5e5
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-10-20 05:23:40 -0500

    changing blog implementation (no firebase)

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,106 +1,18 @@
 import './Blog.css';
-import React, { useRef, useState } from 'react';
-import { firebaseConfig } from '../fbase';
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-
-//import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-firebase.initializeApp(firebaseConfig);
-//const auth = firebase.auth();
-const db = firebase.firestore();
+import React from 'react';
 
 const Blog = () => {
-  //const [user] = useAuthState(auth);
   return (
     <div id='blog-wrap'>
       <div id='blog-header-wrap'>
         <h3>re detach blog</h3>
-        {/*<SignOut />*/}
       </div>
       <div id='blog-body-wrap'>
-        {/*user ? <BlogPosts /> : <SignIn/>*/}
-        <BlogPosts />
+        <p>I just deleted the blog. Starting over raw dogging the HTML as Prime likes to say. But this is still react.js so frowning is justified.</p>
+        <p>Lots more to come.</p>
       </div>
     </div>
   );
-}
-
-/*function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return (
-    <div id='signin-wrap'>
-      <button id='signin-btn' onClick={signInWithGoogle}>Sign In With Google</button>
-    </div>
-  );
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <div id='signout-wrap'>
-      <button id='signout-btn' onClick={() => auth.signOut()}>Sign Out</button>
-    </div>
-  );
-}*/
-
-function BlogPosts() {
-  const dummy = useRef();
-  const postsRef = db.collection('posts');
-  const query = postsRef.orderBy('createdAt').limit(25);
-  const [posts] = useCollectionData(query, { idField: 'id'});
-  //const [formValue, setFormValue] = useState('');
-  
-  /*const createPost = async(e) => {
-    e.preventDefault();
-    //const { uid, photoURL } = auth.currentUser;
-    await postsRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid: uid,
-      photoURL: photoURL,
-      likes: 0,
-    });
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
-  
-  function PostForm() {
-    return (
-      <form id='post-form' onSubmit={createPost}>
-        <input id='post-text-input' value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder='say something!'/>
-        <button type='submit' disabled={!formValue}>Submit Post</button>
-      </form>
-    );
-  }*/
-  
-  function Post(props) {
-    const { text, uid, createdAt/*, photoURL*/ } = props.content;
-    //const messageClass = uid === auth.currentUser.uid ? 'submitted' : 'posted';
-    return (
-      <div id={/*'message '+messageClass*/uid}>
-        {<img alt='' src={/*photoURL || */'ttps://api.adorable.io/avatars/23/abott@adorable.png'} />}
-        <p>{createdAt && createdAt.toDate().toLocaleString()}</p>
-        <p>{text}</p>
-      </div>
-    );
-  }
-
-  return (
-    <div id='posts-wrap'>
-      <div id='live-posts-wrap'>
-        {posts && posts.map((post) => <Post key={post.id} content={post} />)}
-        {<span ref={dummy}></span>}
-      </div>
-      {/*auth.currentUser && (auth.currentUser.email === 'rmbmusicmgmt@gmail.com') ? <PostForm /> : <></>*/}
-    </div>
-  ); 
 }
 
 export default Blog;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div id='contact-wrap' className='page-wrap'>
-      <p>For now try and contact me through instagram DM (@re_detach) or in any comment section.</p>
-      <p>Lets make something nobody could ever imagine.</p>
+      <p>@re_detach on all social media</p>
     </div>
   );
 }
