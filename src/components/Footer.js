23d902dc016ed292f29417703e758cd6f9483288
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-08 15:15:31 -0600

    spotify soon!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,12 @@ const Footer = () => {
   return (
     <footer id='footer-wrap'>
       <ul id='footer-links'>
-        <li key='twitch'><a href='https://www.twitch.tv/re_detach'>Twitch.tv</a></li>
         <li key='youtube'><a href='https://www.youtube.com/@re_detach'>YouTube</a></li>
         <li key='instagram'><a href='https://www.instagram.com/re_detach/'>Instagram</a></li>
         <li key='soundcloud'><a href='https://www.soundcloud.com/re_detach'>Soundcloud</a></li>
+        <li key='spotify'><a href='https://www.spotify.com/re_detach'>Spotify</a></li>
         <li key='tiktok'><a href='https://www.tiktok.com/@re_detach'>TikTok</a></li>
+        <li key='twitch'><a href='https://www.twitch.tv/re_detach'>Twitch.tv</a></li>
       </ul>
     </footer>
   )
