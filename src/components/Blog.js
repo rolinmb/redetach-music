da3fba911ed465dee32b7cb94f17be0df39ea539
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-08 15:13:45 -0600

    charli propaganda

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,10 +5,11 @@ const Blog = () => {
   return (
     <div id='blog-wrap'>
       <div id='blog-header-wrap'>
-        <h3>dumb blog</h3>
+        <h3>charli's mental prison blog</h3>
       </div>
       <div id='blog-body-wrap'>
         <p>11/5/2024 :: Dear void, I am calling to you to give me moneys. Thank you if you are reading this. Thank you thank you thank you thank you thank you if you help me get moneys. I hope you get moneys too.</p>
+        <p>11/8/2024 :: Dear charli, I keep thinking about how I thought I saw your face and your mother a few times in Chicago yesterday. But I am sad I never did anything about it. You're so fucking pretty. I'm not going to London until you have your shows; that means we have a few weeks to get to know eachother in person I hope.</p>
       </div>
     </div>
   );
