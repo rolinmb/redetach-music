968e1d454457251f443b412df57323ac7cc0574c
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-20 04:50:08 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,6 +10,7 @@ const Blog = () => {
       <div id='blog-body-wrap'>
         <p>11/18/2024 :: rubber pencils suck, iykyk, lgtbrq charli i know the typa worq, iaintgoinanywhere</p>
         <p>11/19/2024 :: demons tryna get to me; need charli's angels to take me by tha hand so we can get this bred</p>
+        <p>11/20/2024 :: we cookin wit antimatter nobody understands the typa vibes that r otw</p>
       </div>
     </div>
   );
