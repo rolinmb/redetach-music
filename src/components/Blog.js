0282e16db9d357c1fc120f492f307c54424fc9d6
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-21 23:28:39 -0600

    playtience

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,6 +12,7 @@ const Blog = () => {
         <p>11/19/2024 :: demons tryna get to me; need charli's angels to take me by tha hand so we can get this bred</p>
         <p>11/20/2024 :: we cookin wit antimatter nobody understands the typa vibes that r otw</p>
         <p>11/21/2024 :: whats so bad about crashing out if everyone already knows everything about you</p>
+        <p>11/22/2024 :: i guess yall just had 2 rlly b there smh</p>
       </div>
     </div>
   );
