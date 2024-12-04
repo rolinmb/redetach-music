87b57cf492e5e4b47831bb385f233fbddbb53591
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-04 13:26:50 -0600

    blogq updatre

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,15 +8,7 @@ const Blog = () => {
         <h3>charli's mental prison blog</h3>
       </div>
       <div id='blog-body-wrap'>
-        <p>11/18/2024 :: rubber pencils suck, iykyk, lgtbrq charli i know the typa worq, iaintgoinanywhere</p>
-        <p>11/19/2024 :: demons tryna get to me; need charli's angels to take me by tha hand so we can get this bred</p>
-        <p>11/20/2024 :: we cookin wit antimatter nobody understands the typa vibes that r otw</p>
-        <p>11/21/2024 :: whats so bad about crashing out if everyone already knows everything about you</p>
-        <p>11/22/2024 :: i guess yall just had 2 rlly b there smh</p>
-        <p>11/23/2024 :: shoutout saint michael, also might just runnit baq this morning. hopefully my parents waste the cops time and resources again stopping me</p>
-        <p>11/24/2024 :: welcome to new hollywood. dumb live remixes should all be on soundcloud by eod hopefully.</p>
-        <p>           :: charli xcx used 'eminent domain' on my neighborhood so we can live in peace</p>
-        <p>           :: still waiting on these worthless bad actors to just drive the f away so we all can make money. they are holding up the future because of their pussy egos, just drive away now and nothing bad will happen. OPEN YOUR EYES READ THE SCRIPT OR DIE A SHITTY ACTOR.</p>
+        <p>12/4/2024 :: I had a dream about charli once the THC exited my system</p>
       </div>
     </div>
   );
