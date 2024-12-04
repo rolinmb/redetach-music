4cbead27c7678d9071ea626af3b0ac357ee1458a
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-04 14:44:23 -0600

    small update

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Shows = () => {
   return (
     <div id="shows-wrap" className='page-wrap'>
-      <p><a href='https://www.charlixcx.com/'>catch me at charli's shows innafew weeks and in the US next spring; maybe in mexico too... LETS GO!</a></p>
+      <p><a href='https://www.charlixcx.com/'>catch me at charli's shows in the US next spring; maybe in mexico too... LETS RIDE! vroom vroom</a></p>
     </div>
   );
 }
