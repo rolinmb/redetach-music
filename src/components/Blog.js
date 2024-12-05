471a3204ad11b14709fc36f8f6cc9421b2658cc7
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-05 12:23:51 -0600

    charli now Charlotte

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,11 +5,11 @@ const Blog = () => {
   return (
     <div id='blog-wrap'>
       <div id='blog-header-wrap'>
-        <h3>charli's mental prison blog</h3>
+        <h3>Charlotte's Mental Prison Blog</h3>
       </div>
       <div id='blog-body-wrap'>
         <p>12/4/2024 :: I had a dream about Charlotte once the THC exited my system</p>
-        <p>12/5/2024 :: redetach remixes set 2 going live on youtube in a little bit; getting ready for dumb live remixes set 5 next</p>
+        <p>12/5/2024 :: redetach remixes set 2 going live on youtube around/before 1:30PM CST; getting ready for dumb live remixes set 5 next</p>
       </div>
     </div>
   );
