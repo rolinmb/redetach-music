70aba436ebc53573fd082fc74792c8010f979edf
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-09 13:30:47 -0600

    dumb

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,7 +12,7 @@ const Blog = () => {
         <p>12/5/2024 :: redetach remixes set 2 going live on youtube around/before 1:30PM CST; getting ready for dumb live remixes set 5 next</p>
         <p>12/6/2024 :: the world needs to create a positive/optimistic feedback loop</p>
         <p>12/7/2024 :: go outside</p>
-        <p>12/8/2024 :: $30k in medical/college debt; unemployed too</p>
+        <p>12/8/2024 :: $17.5k in medical debt; $20k in college debt; unemployed too</p>
         <p>12/9/2024 :: nothing new today, just trying to make moneys</p>
       </div>
     </div>
