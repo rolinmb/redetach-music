b167fb48b18559cc07f9e9667d5e125c2ba03b14
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-11 08:21:43 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,13 +8,14 @@ const Blog = () => {
         <h3>Charlotte's Mental Prison Blog</h3>
       </div>
       <div id='blog-body-wrap'>
-        <p>12/4/2024 :: I had a dream about Charlotte once the THC exited my system</p>
+        <p>12/4/2024 :: i had a dream about Charlotte once the THC exited my system</p>
         <p>12/5/2024 :: redetach remixes set 2 going live on youtube around/before 1:30PM CST; getting ready for dumb live remixes set 5 next</p>
         <p>12/6/2024 :: the world needs to create a positive/optimistic feedback loop</p>
         <p>12/7/2024 :: go outside</p>
         <p>12/8/2024 :: $17.5k in medical debt; $20k in college debt; unemployed too</p>
         <p>12/9/2024 :: all my iphone notes got magically deleted xd idgaf if im used as propaganda, just make sure i get paid asap</p>
         <p>12/10/2024 :: if you havent checked out my live remixes on my soundcloud you're missing out</p>
+        <p>12/11/2024 :: i miss you paige, i am sorry and i wish i could talk to you again</p>
       </div>
     </div>
   );
