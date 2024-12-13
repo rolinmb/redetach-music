63ef5bca895e3f6faa03d9bda73a0e3eab3e8639
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-12 19:51:43 -0600

    removing charlotte

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,11 +5,10 @@ const Blog = () => {
   return (
     <div id='blog-wrap'>
       <div id='blog-header-wrap'>
-        <h3>Charlotte's Mental Prison Blog</h3>
+        <h3>the blog</h3>
       </div>
       <div id='blog-body-wrap'>
-        <p>12/11/2024 :: i miss you paige, i am sorry and i wish i could talk to you again</p>
-        <p>12/12/2024 :: i can't listen to charli xcx anymore because my ego gets too big</p>
+        <p>12/13/2024 :: working on singing lately</p>
       </div>
     </div>
   );
