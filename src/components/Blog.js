7c000cb11f0440318b80559c57fc6b45e27518a3
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-14 16:29:45 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,7 +8,8 @@ const Blog = () => {
         <h3>the blog</h3>
       </div>
       <div id='blog-body-wrap'>
-        <p>12/13/2024 :: working on singing lately</p>
+        <p>12/13/2024 :: working on vocals lately</p>
+        <p>12/14/2024 :: my next project will have some songs with lyrics and some just instrumentals like um (MOMS BASEMENT SET)</p>
       </div>
     </div>
   );
