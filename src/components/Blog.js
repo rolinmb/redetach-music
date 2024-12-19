7d5dc6ed7ef5b3760b3420230b248d73d9c4918d
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-19 10:39:04 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,8 +12,9 @@ const Blog = () => {
         <p>12/14/2024 :: the next project will have some songs with lyrics and some just instrumentals like um (MOMS BASEMENT SET)</p>
         <p>12/15/2024 :: who cares about crashing out if internet privacy is already a joke</p>
         <p>12/16/2024 :: 8 songs on the next project, still figuring out a project title</p>
-        <p>12/17/2024 :: streaming making new music today as soon as i find motivation</p>
+        <p>12/17/2024 :: streaming making new music whenever i find motivation</p>
         <p>12/18/2024 :: made some more dance/house style beats lately, added some guitar and it ended up neat</p>
+        <p>12/19/2024 :: zynbabwe is my current location</p>
       </div>
     </div>
   );
