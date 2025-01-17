c79772b62af2cd495d222613076424ea8882b51b
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2025-01-17 16:01:58 -0600

    dumb live remixes 11

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,8 @@ const Projects = () => {
     <div id='yt-playlists-wrap' className='page-wrap'>
       <h2>dumb live remix sets on soundcloud</h2>
       <div id='iframes-wrap-sc'>
+        <h4>set 11 // 1/17/2025</h4>
+        <iframe frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2011491595&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
         <h4>set 10 // 1/11/2025</h4>
         <h5>playlist <a href="https://youtube.com/playlist?list=PL411TIk1YJznLMzK2JUrL486FSiWkfdJY&feature=shared">here</a></h5>
         <iframe frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2006976227&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
