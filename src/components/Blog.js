272eaa8e50e3a9fb7bc20855e49a6e88ffd5c98e
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2025-01-19 11:13:19 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -44,6 +44,7 @@ const Blog = () => {
         <p>1/16/2025  :: but never forgetti</p>
         <p>1/17/2025  :: positive feedback loop now on my website, on youtube as a playlist, and on soundcloud very soon</p>
         <p>1/18/2025  :: so many likes on one of my videos thank yous</p>
+        <p>1/19/2025  :: keep listening to my musiq i dare you</p>
       </div>
     </div>
   );
