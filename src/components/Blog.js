5d0455d0ad41974ac0c53bd17f40d361d18f0d50
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2025-01-21 11:28:57 -0600

    blog update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -46,6 +46,7 @@ const Blog = () => {
         <p>1/18/2025  :: so many likes on one of my videos thank yous</p>
         <p>1/19/2025  :: keep listening to my musiq i dare you</p>
         <p>1/20/2025  :: the name of my next project is 'reflect'</p>
+        <p>1/21/2025  :: grimey</p>
       </div>
     </div>
   );
